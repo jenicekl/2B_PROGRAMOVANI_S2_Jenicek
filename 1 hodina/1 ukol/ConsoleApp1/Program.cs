981fefa738f25343d1719ffbb9703d8dddc52df2
jenicekl <lukasjena9@gmail.com>
2025-09-19 12:33:27 +0200

    5

---
+++ 1 hodina/1 ukol/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] pole = new int[10];
+
+            for (int i = 0; i < pole.Length; i++)
+            {
+                pole[i] = rand.Next(1, 101);
+            }
+
+            Console.Write("obsah pole:");
+            foreach (int x in pole)
+            {
+                Console.Write(x + ", ");
+            }
+            Console.WriteLine();
+
+            int min = int.MaxValue;
+            int max = 0;
+
+            for (int i = 0; i < pole.Length; i++)
+            {
+                if (pole[i] < min)
+                {
+                    min = pole[i];
+                }
+                if (pole[i] > max)
+                {
+                    max = pole[i];
+                }
+            }
+
+            Console.WriteLine($"nejmensi hodnota: {min}");
+            Console.WriteLine($"nejvetsi hodnota: {max}");
+
+            double soucet = 0;
+            for (int i = 0; i < pole.Length; i++)
+            {
+                soucet += pole[i];
+            }
+            double prumer = soucet / pole.Length;
+            Console.WriteLine($"prumer: {prumer}");
+
+            Array.Sort(pole);
+
+            Console.WriteLine("serazene pole:");
+            foreach (int x in pole)
+            {
+                Console.Write(x + ", ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ 1 hodina/2 ukol/2 ukol/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _2_ukol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[,] pole = new int[5, 5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    pole[i,j] = rand.Next(1, 51);
+                }
+            }
+
+            Console.WriteLine("Pole: ");
+            for (int i = 0;i < 5; i++)
+            {
+                for(int j = 0;j < 5; j++)
+                {
+                    Console.Write(pole[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if(min > pole[i, j])
+                    {
+                        min = pole[i,j];
+                    }
+                    if(max < pole[i, j])
+                    {
+                        max = pole[i,j];
+                    }
+                }
+            }
+            
+            Console.WriteLine($"Min: {min}");
+            Console.WriteLine($"Max: {max}");
+
+
+            int soucet = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    soucet += pole[i, j];
+                }
+            }
+            Console.WriteLine($"Soucet vsech cisel je: {soucet}");
+
+
+            Console.WriteLine("Hlavni diagonal: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(pole[i,i] + " ");
+            }
+        }
+    }
+}
