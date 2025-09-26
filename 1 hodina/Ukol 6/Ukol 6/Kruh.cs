da41d7bb8fd95dece275587854ef32ab5140d977
jenicekl <lukasjena9@gmail.com>
2025-09-26 12:23:10 +0200

    da

---
+++ 1 hodina/Ukol 6/Ukol 6/Kruh.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ukol_6
+{
+    internal class Kruh
+    {
+        public double Polomer;
+
+        public Kruh(double polomer)
+        {
+            Polomer = polomer;
+        }
+        public double VypocitejObvod()
+        {
+            return 2 * Math.PI * Polomer;
+        }
+
+        public double VypoctejObsah()
+        {
+            return Math.PI * Polomer * Polomer;
+        }
+
+    }
+}
---
+++ 1 hodina/Ukol 6/Ukol 6/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ukol_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kruh kruh = new Kruh(5);
+            Console.WriteLine("Obvod:" + kruh.VypocitejObvod());
+            Console.WriteLine("Obsah:" + kruh.VypoctejObsah());
+        }
+    }
+}
